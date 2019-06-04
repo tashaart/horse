909c3944c0a82392ce16c39a949d8e746c27d36a
Tasha Art <nataly.j7.17@gmail.com>
2019-06-05 00:06:10 +0700

    version two

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 namespace horse
 {
     class Program
@@ -7,16 +6,15 @@ namespace horse
         static void Main(string[] args)
         {
             var input = Console.ReadLine();
-            char[] alph = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
-            int a = Array.IndexOf(alph, input[0]) + 1;
+            int a = (int)input[0];
             int b = (int)char.GetNumericValue(input[1]);
             for (int i = 1; i < 9; i++)
             {
                 for (int j = 1; j < 9; j++)
                 {
-                    if ((Math.Abs(i - a) == 1 && Math.Abs(j - b) == 2) || (Math.Abs(i - a) == 2 && Math.Abs(j - b) == 1))
+                    if ((Math.Abs(i+96 - a) == 1 && Math.Abs(j - b) == 2) || (Math.Abs(i+96 - a) == 2 && Math.Abs(j - b) == 1))
                     {
-                        Console.WriteLine(Convert.ToString(alph[i - 1]) + Convert.ToString(j));
+                        Console.WriteLine(Convert.ToChar(96+i) + Convert.ToString(j));
                     }
                 }
             }
